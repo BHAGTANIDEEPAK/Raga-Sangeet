b6d65c104b376d13c0ae0b80d4d164fd08005968
BHAGTANIDEEPAK <107862278+BHAGTANIDEEPAK@users.noreply.github.com>
2023-08-01 10:54:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -96,4 +96,15 @@ function buttonAnimation(currentKey){
         activeButton.classList.remove("pressed");
     },100);
 }
+var i = 0;
+var txt = 'Raga🎵Sangeet';
+var speed = 100;
 
+function typewriter(){
+    if(i<txt.length){
+        document.getElementById("title").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typewriter,speed);
+        }
+}
+typewriter();
